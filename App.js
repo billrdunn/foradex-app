a49c61ef8019f34b251d1e137093cde8d5b826f2
Bill Dunn <billrdunn@gmail.com>
2022-05-30 09:16:39 +0100

    Set up project so it works with react-native-debugger.

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+// http://localhost:19000/debugger-ui/
 
 export default function App() {
+  console.log("hello!");
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
